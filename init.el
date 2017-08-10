1f7d0db60417df06123fc590c7197579f6222b5f
QBJORT <qbjort@gmail.com>
2017-08-10 18:29:28 +0200

    Added config for org mode, few tweeks to init

--- init.el
+++ init.el
@@ -1,4 +1,8 @@
-;; Package Setup
+;; Startup
+(setq inhibit-splash-screen t ;;prevents the default welcom screen
+      inhibit-startup-message t);;hide the startup message (usually annoying)
+
+;; Package (configures the repository system)
 (require 'package);; source of plugins in emacs
 (setq package-archives ;;set the source of remote directories
       '(("marmalade" . "http://marmalade-repo.org/packages/")
@@ -13,15 +17,50 @@
 ;; Use-package bootstrap (use-package is macro for configuring other packages)
 (unless (package-installed-p 'use-package);;check if use package exsists
   (package-install 'use-package));; automatically install usepackage
+(setq use-package-always-ensure t);;
 
-;; Customization startup
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t) ;; hide the startup message (usually annoying)
-
-;; Backups (and recovery) 
-(setq backup-inhibited t ;; inhibits backups
-      make-backup-files nil ;; prevent backup files 
-      auto-save-list-file-prefix nil ;; prents auto-save-history directory
+;; Backups (and recovery disabled) 
+(setq backup-inhibited t ;;inhibits backups
+      make-backup-files nil ;;prevent backup files 
+      auto-save-list-file-prefix nil ;;prevents auto-save-history directory
       auto-save-default nil)
 
-;; Window
+;; UI (sets up frame)
+(when (display-graphic-p);;when graphical interface
+;maximise frame realest
+  (scroll-bar-mode 0);;remove the scroll bar
+  (tool-bar-mode 0);;disables the tool-bar
+  (menu-bar-mode 0));;disables the menu-bar
+
+;; Window geometry (prevents a 'white' line from appearing)
+(add-to-list 'default-frame-alist '(height . 40))
+(add-to-list 'default-frame-alist '(width . 90))
+
+;; Server (impoved startup timing)
+(unless (server-running-p) 
+  (server-start));;starts the emacs --daemond
+;Increase the garbage collection threshold (500mb)
+(setq gc-cons-threshold (* 500 1024 1024))
+
+;; UTF-8 encoding
+; make sure that UTF-8 (unicode) is used everywhere
+(prefer-coding-system        'utf-8)
+(set-terminal-coding-system  'utf-8)
+(set-keyboard-coding-system  'utf-8)
+(set-language-environment    'utf-8)
+(set-selection-coding-system 'utf-8)
+(setq locale-coding-system   'utf-8)
+
+;; Org-bable setup for mordern ORG config
+(use-package org
+  :ensure t ;;make sure its install (usually a default package)
+  :config
+  ;fix indentation problem with code blocks
+  (setq org-src-preserve-indentation nil 
+	org-edit-src-content-indentation nil)
+  ;fix syntax native for native syntax highlighting and tabs
+  (setq org-src-fontify-natively t
+	org-src-tab-acts-natively t)
+  ;; Load config.org -> Emacs configuration (with in a org document)
+  (org-babel-load-file (concat user-emacs-directory "config.org")))
+
